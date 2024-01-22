f0d37b914640132d99fcb22d2b2488d22af73ecd
Takenoko3333 <153407565+Takenoko3333@users.noreply.github.com>
2024-01-23 07:03:16 +0900

    Added ability to save export settings
    Added ability to save export settings to local storage
    書き出し設定のローカルストレージ保存機能の追加

--- Danbooru-Tags-Exporter.user.js
+++ Danbooru-Tags-Exporter.user.js
@@ -8,7 +8,7 @@
 // @supportURL   https://github.com/Takenoko3333/Danbooru-Tags-Sort-Exporter/issues
 // @homepageURL  https://github.com/Takenoko3333/Danbooru-Tags-Sort-Exporter
 // @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
-// @version      0.4.2
+// @version      0.5.0
 // @description  Select specified tags and copy to clipboard, for Stable Diffusion WebUI or NovelAI to use. Tags can be sorted by tag order in NovelAI method.
 // @description:zh-TW  選擇指定標籤並複製到剪貼板，供Stable Diffusion WebUI或NovelAI等使用。標籤可根據 NovelAI 的標籤排序方法進行排序。
 // @description:zh-HK  選擇指定標籤並複製到剪貼板，供Stable Diffusion WebUI或NovelAI等使用。標籤可根據 NovelAI 的標籤排序方法進行排序。
@@ -23,6 +23,8 @@
 // @grant        GM.notification
 // @grant        GM.addStyle
 // @license      AGPL-3.0
+// @downloadURL https://update.greasyfork.org/scripts/484998/Danbooru%20Tags%20Select%20to%20Sort%20and%20Export.user.js
+// @updateURL https://update.greasyfork.org/scripts/484998/Danbooru%20Tags%20Select%20to%20Sort%20and%20Export.meta.js
 // ==/UserScript==
 
 // Forked from FSpark/Danbooru-Tags-Exporter(https://github.com/FSpark/Danbooru-Tags-Exporter)
@@ -30,55 +32,68 @@
 (function () {
     'use strict';
 
-    // ==Setting==
-    const sortInitial = true; // Sorted by tag order in NovelAI method (タグをNovelAI方式で並び替える)
-    const bracketEscapeInitial = false; // true: Stable Diffusion, false: NovelAI
-    const roundBracketsSelectInitial = 1; // 0: ( ) Stable Diffusion, 1: { } NovelAI
-    // ==/Setting==
+    // Settings changed on the web page are saved to local storage and applied when revisiting.
+    // ウェブページ上で変更した設定はローカルストレージに保存され、再訪時に適用されます。
 
-    let bracketEscapeCheck = "";
+    // ==Settings==
+    const initialSettings = {
+        sort: true, // Sorted by tag order in NovelAI method. (タグをNovelAI方式で並び替える)
+        bracketEscape: false, // true: Stable Diffusion, false: NovelAI
+        setWeight: false, // true: Show and Activate weight inputs. (ウェイト入力の表示と有効化)
+        useBracket: 1 // 0: ( ) Stable Diffusion, 1: { } NovelAI
+    };
+    // ==/Settings==
+
+    let settings = {};
+    let localSettings = JSON.parse(localStorage.getItem("settings"));
+    settings = localSettings || initialSettings;
+
     let sortCheck = "";
-    let roundBracketsCheck0 = "";
-    let roundBracketsCheck1 = "";
-    let roundBracketsValue0 = -1;
-    let roundBracketsValue1 = -1;
+    let bracketEscapeCheck = "";
+    let setWeightCheck = "";
+    let roundBracketsRadio = "";
+    let curlyBracketsRadio = "";
 
-    if(bracketEscapeInitial) {
-        bracketEscapeCheck = "checked";
-    }
-    if(sortInitial) {
+    if(settings.sort) {
         sortCheck = "checked";
     }
-    if(roundBracketsSelectInitial) {
-        roundBracketsCheck1 = "checked";
-        roundBracketsValue1 = 0;
+    if(settings.bracketEscape) {
+        bracketEscapeCheck = "checked";
+    }
+    if(settings.setWeight) {
+        setWeightCheck = "checked";
+    }
+    if(settings.useBracket) {
+        curlyBracketsRadio = "checked";
     } else {
-        roundBracketsCheck0 = "checked";
-        roundBracketsValue0 = 0;
+        roundBracketsRadio = "checked";
     }
 
     GM.addStyle(`#tags-exporter-setting button, #tag-list button {margin: 0.25rem 0 0.5rem 0; padding: 0.25em 0.75em;}
-                #tags-exporter-setting label{margin: .25em; line-height: 1.5em;}
-               .tag-weight {width: 3em; margin-left: .25em}`);
+                 #tags-exporter-setting button#reset-settings {margin-top: .5em}
+                 #tags-exporter-setting label{margin: .25em; line-height: 1.5em;}
+                 .tag-weight {width: 3em; margin-left: .25em}`);
 
     let SettingPanel = document.createElement('section');
     SettingPanel.id = "tags-exporter-setting";
     SettingPanel.innerHTML = `
-      <h2>Tags Export Settings</h2>
-<input type="checkbox" id="sort" ${sortCheck}/><label for="sort">Sort by NovelAI method</label><br>
-<input type="checkbox" id="bracket-escape" ${bracketEscapeCheck}/><label for="bracket-escape"><code>(</code> <code>)</code> -> <code>\\(</code> <code>\\)</code></label><br>
-<input type="checkbox" id="set-weight" /><label for="set-weight">Setting weights</label><br>
-<div>
- <input type="radio" name="use-bracket" id="round-brackets" value="${roundBracketsValue0}" ${roundBracketsCheck0}/><label for="round-brackets">Using ( )</label>
-<input type="radio" name="use-bracket" id="curly-brackets" value="${roundBracketsValue1}" ${roundBracketsCheck1}/><label for="curly-brackets">Using { }</label></div>
-    `
+        <h2>Tags Export Settings</h2>
+        <input type="checkbox" id="sort" ${sortCheck}/><label for="sort">Sort by NovelAI method</label><br>
+        <input type="checkbox" id="bracket-escape" ${bracketEscapeCheck}/><label for="bracket-escape"><code>(</code> <code>)</code> -> <code>\\(</code> <code>\\)</code></label><br>
+        <input type="checkbox" id="set-weight"  ${setWeightCheck}/><label for="set-weight">Setting weights</label><br>
+        <div>
+        <input type="radio" name="use_bracket" id="round-brackets" value="0" ${roundBracketsRadio}/><label for="round-brackets">Using ( )</label>
+        <input type="radio" name="use_bracket" id="curly-brackets" value="1" ${curlyBracketsRadio}/><label for="curly-brackets">Using { }</label></div>
+        <button name="reset_settings" id="reset-settings">Settings Reset</button>
+        `
 	let Container = document.createElement('div');
     Container.id = "tags-exporter-container";
-    Container.innerHTML = `<button name="select_all">All</button>
-<button  name="select_none">None</button>
-<button  name="invert_select">Invert</button>
-<button  name="export">Export</button>
-`
+    Container.innerHTML = `
+        <button name="select_all">All</button>
+        <button name="select_none">None</button>
+        <button name="invert_select">Invert</button>
+        <button name="export">Export</button>
+        `
 
 	function insertAfter(newNode, referenceNode) {
         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
@@ -89,8 +104,9 @@
 
     function addBrackets(prompts,isRound,n){
         let l,r;
-        if(n==0)
+        if(n==0) {
             return prompts
+        }
         else if(n>0){
             if(isRound){
                 l='('
@@ -109,10 +125,10 @@
     function exportTags(target){
         let tags = []
         let reorderedTags = []
+        let sort = document.getElementById("sort").checked
         let bracket_escape = document.getElementById("bracket-escape").checked
         let set_weight = document.getElementById("set-weight").checked
         let round_brackets = document.getElementById("round-brackets").checked
-        let sort = document.getElementById("sort").checked
         if(!target) {
             if(sort) {
                 ["[name=character-tags]:checked","[name=copyright-tags]:checked","[name=artist-tags]:checked","[name=general-tags]:checked"].forEach((t)=>createTags(t));
@@ -125,9 +141,10 @@
         function createTags(target) {
             document.querySelectorAll(target).forEach((e) => {
                 let prompts = e.value
-                if(bracket_escape)
+                if(bracket_escape) {
                     prompts = prompts.replaceAll(`(`,`\\(`).replaceAll(`)`,`\\)`)
-                if(set_weight){
+                }
+                if(set_weight) {
                     prompts = addBrackets(prompts,round_brackets,e.nextSibling.value)
                 }
                 tags.push(prompts)
@@ -198,8 +215,40 @@
         };
     }
 
+    function setSettings() {
+        let sort = document.getElementById("sort").checked;
+        let bracketEscape = document.getElementById("bracket-escape").checked;
+        let setWeight = document.getElementById("set-weight").checked;
+        let roundBrackets = document.getElementById("round-brackets").checked;
+        let useBracket = roundBrackets ? 0 : 1;
+        localStorage.setItem("settings", JSON.stringify({sort: sort, bracketEscape: bracketEscape, setWeight: setWeight, useBracket: useBracket}));
+        console.log(JSON.parse(localStorage.getItem("settings")));
+    }
+
+    function resetSettings() {
+        localStorage.removeItem("settings");
+        document.getElementById("sort").checked = initialSettings.sort;
+        document.getElementById("bracket-escape").checked = initialSettings.bracketEscape;
+        document.getElementById("set-weight").checked = initialSettings.setWeight;
+        if(initialSettings.useBracket) {
+            document.getElementById("curly-brackets").checked = true;
+        } else {
+            document.getElementById("round-brackets").checked = true;
+        }
+        toggleWeightInputs();
+    }
+
+    function toggleWeightInputs(event) {
+        const target = event ? event.target : document.getElementById('set-weight');
+        if (target && target.id === 'set-weight') {
+            const isSetWeightChecked = target.checked;
+            document.querySelectorAll("#tag-list input[type='number']").forEach(e => { e.hidden = !isSetWeightChecked; });
+        }
+    }
+
     ["artist-tag","character-tag","copyright-tag","general-tag"].forEach((t)=>insertButtons(t))
 
+    toggleWeightInputs();
 
     Container.querySelector("[name='select_all']").onclick = function () {
         var items = document.querySelectorAll("#tag-list input[type='checkbox']")
@@ -222,13 +271,14 @@
     Container.querySelector("[name='export']").onclick = function () {
         exportTags()
     };
-
-    document.getElementById("set-weight").onchange = function (e){
-        if(e.target.checked){
-            document.querySelectorAll("#tag-list input[type='number']").forEach((e)=>e.hidden=false)
-        }else{
-            document.querySelectorAll("#tag-list input[type='number']").forEach((e)=>e.hidden=true)
-        }
-    }
+    SettingPanel.querySelector("[name='reset_settings']").onclick = function () {
+        resetSettings()
+    };
+    SettingPanel.querySelectorAll("input[type='radio'], input[type='checkbox']").forEach(e => {
+        e.onchange = function (event) {
+            setSettings();
+            toggleWeightInputs(event);
+        };
+    });
 
 })();
